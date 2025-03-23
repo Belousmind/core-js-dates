8cf607082df6213251add9269f5f63b5b9e37aa7
belousmind <belousmind@gmail.com>
2025-03-23 10:50:52 +0300

    feat: solve getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -52,8 +52,11 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const dateParse = new Date(date);
+  const options = { weekday: 'long' };
+  const dayOfWeek = dateParse.toLocaleDateString('en-US', options);
+  return dayOfWeek;
 }
 
 /**
